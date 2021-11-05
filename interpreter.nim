b06c154f1e79f2676e57053dd43b8f7ecb787339
Inspiaaa <lavaafterburner@gmail.com>
2021-11-05 21:32:18 +0100

    Move code into a reusable function

--- interpreter.nim
+++ interpreter.nim
@@ -1,7 +1,9 @@
 
 import times
+import std/streams
 
-const code: string = readFile("bf/mandelbrot.bf")
+
+# 128.77s for mandelbrot
 
 
 proc createJumpTable(code: string): seq[int] =
@@ -21,57 +23,53 @@ proc createJumpTable(code: string): seq[int] =
     return jumpTable
 
 
-const input: string = "Hello"
-var input_idx = 0
+proc run(code: string; input, output: Stream) =
+    var tape: seq[uint8] = @[0u8]
+    let jumpTable: seq[int] = createJumpTable(code)
+
+    var codePos: int = 0
+    var tapePos: int = 0
+
+    while codePos < len(code):
+        let c: char = code[codePos]
+
+        # echo c, " Tape ", tape, len(tape)
+
+        case c
+        of '>':
+            inc tapePos
+            if tapePos >= len(tape):
+                tape.add(0)
+
+        of '<': dec tapePos
+        of '+': inc tape[tapePos]
+        of '-': dec tape[tapePos]
+        of '.':
+            output.write char(tape[tapePos])
+
+        of ',':
+            var input_char: uint8
+            if input.atEnd:
+                input_char = 0
+            else:
+                input_char = uint8(input.readChar())
+            tape[tapePos] = input_char
+
+        of '[':
+            if tape[tapePos] == 0:
+                codePos = jumpTable[codePos]
+
+        of ']':
+            if tape[tapePos] != 0:
+                codePos = jumpTable[codePos]
+
+        else: discard
+        inc codePos
 
 
-var tape: seq[uint8] = @[0u8]
-let jumpTable: seq[int] = createJumpTable(code)
-
-
-var codePos: int = 0
-var tapePos: int = 0
-
+let code: string = readFile("bf/mandelbrot.bf")
 let startTime = epochTime()
-
-while codePos < len(code):
-    let c: char = code[codePos]
-
-    # echo c, " Tape ", tape, len(tape)
-
-    case c
-    of '>':
-        inc tapePos
-        if tapePos >= len(tape):
-            tape.add(0)
-
-    of '<': dec tapePos
-    of '+': inc tape[tapePos]
-    of '-': dec tape[tapePos]
-    of '.':
-        stdout.write char(tape[tapePos])
-        # stdout.write tape[tapePos], " "
-
-    of ',':
-        var input_char: uint8
-        if input_idx < len(input):
-            input_char = uint8(input[input_idx])
-        else:
-            input_char = 0
-        tape[tapePos] = input_char
-
-    of '[':
-        if tape[tapePos] == 0:
-            codePos = jumpTable[codePos]
-
-    of ']':
-        if tape[tapePos] != 0:
-            codePos = jumpTable[codePos]
-
-    else: discard
-    inc codePos
-
-
+run(code, newStringStream("Hello"), newFileStream(stdout))
 let elapsedTime = epochTime() - startTime
 echo()
 echo "Time: ", elapsedTime
