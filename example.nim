612c4a387985005a3602c5ce315d39d69eb94fa1
Inspiaaa <lavaafterburner@gmail.com>
2021-11-14 18:47:58 +0100

    Create a code example for usage of the API

---
+++ example.nim
@@ -0,0 +1,30 @@
+
+import std/macros
+import std/streams
+import times
+
+import ./src/optimizing_interpreter
+
+
+macro timeit(message: string, code: untyped): untyped =
+    result = quote do:
+        let startTime = epochTime()
+        `code`
+        let elapsedTime = epochTime() - startTime
+        echo()
+        echo `message`, " ", elapsedTime, "s"
+
+
+let code = readFile("examples/mandelbrot.bf")
+
+timeit "Parse time:":
+    var instructions = parse(code)
+    instructions = optimize(instructions)
+    addJumpInformation(instructions)
+
+# Inspect the optimised instructions:
+# for i in instructions[0..<min(50, len(instructions))]:
+#     echo i
+
+timeit "Execution time:":
+    run(instructions, newStringStream("10"), newFileStream(stdout))
