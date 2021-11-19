0116e409e7d928584df865b0584f7ac5b46105aa
Inspiaaa <lavaafterburner@gmail.com>
2021-11-19 17:42:03 +0100

    Add CLI subcommand for inspecting the instructions

--- src/cli.nim
+++ src/cli.nim
@@ -8,20 +8,26 @@ import ./optimizing_interpreter
 import ./naive_interpreter
 
 
-proc bfi(
+template getCode(file: string, code: string): string =
+    if len(file) > 0 and len(code) > 0:
+        stdout.styledWriteLine(fgRed, "CLI Error: Either set file (-f) or code (-c), not both.")
+        return
+
+    if len(file) > 0:
+        readFile(file)
+    else:
+        code
+
+
+proc r(
         file: string = "",
         code: string = "",
         noOpt: bool = false,
         input: string = "",
         output: string = "") =
+    ## Runs the interpreter.
 
-    if len(file) > 0 and len(code) > 0:
-        stdout.styledWriteLine(fgRed, "CLI Error: Either set file (-f) or code (-c), not both.")
-        return
-
-    let code: string = (
-        if len(file) > 0: readFile(file)
-        else: code)
+    let code = getCode(file, code)
 
     let inputStream = (
         if fileExists(input): newFileStream(input, fmRead)
@@ -37,13 +43,39 @@ proc bfi(
         optimizing_interpreter.run(code, inputStream, outputStream)
 
 
-dispatch(
-    bfi,
-    help = {
-        "file": "Path to the file with the BF code",
-        "code": "Directly enter BF code as a string",
-        "noOpt": "Flag to disable optimizations",
-        "input": "Path to the input data; Uses stdin if none specified",
-        "output": "Path to the output file; Uses stdout if none specified"
-    }
+proc inspect(
+        file: string = "",
+        code: string = "",
+        output: string = "",
+        number: int = -1) =
+    ## Prints the first n instructions of the optimized code.
+
+    let code = getCode(file, code)
+    var instructions = optimizing_interpreter.parse(code)
+    instructions = optimizing_interpreter.optimize(instructions)
+    optimizing_interpreter.addJumpInformation(instructions)
+
+    let outputStream = (
+        if len(output) > 0: newFileStream(output, fmWrite)
+        else: newFileStream(stdout))
+
+    let number = if number == -1: len(instructions) else: number
+    for i in 0..<number:
+        outputStream.writeLine(instructions[i])
+
+
+dispatchMulti(
+    [
+        r,
+        help = {
+            "file": "Path to the file with the BF code",
+            "code": "Directly enter BF code as a string",
+            "noOpt": "Flag to disable optimizations",
+            "input": "Path to the input data; Uses stdin if none specified",
+            "output": "Path to the output file; Uses stdout if none specified"
+        }
+    ],
+    [
+        inspect
+    ]
 )
